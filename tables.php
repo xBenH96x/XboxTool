bd5e29eaac79c97f7b714067ba6e4f1f9262b12b
Benjamin <xBenH96x@github.com>
2018-01-13 15:40:40 +0000

    added tables.php (useless until it is useful)

---
+++ tables.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'xbox.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <video src="<?php echo $item['downloadUri']; ?> " controls>
+
+    </video>
+  </body>
+</html>
+
+
+<!--<source src="<?php echo $item['downloadUri']; ?>  "
+         type='video/mp4;codecs="avcl.42E01E, mp4a.40.2"'/>
