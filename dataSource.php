2f2371cfe2be97fa812b45d1f16591fc1e098d1a
xBenH96x <benh96x@gmail.com>
2018-05-29 15:18:03 +0100

    The Jquery Update
    Jquery baby

--- dataSource.php
+++ dataSource.php
@@ -5,20 +5,35 @@ $ch = curl_init ("https://xboxapi.com/v2/2533274839055677/game-clips");
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-AUTH: e5dd6ea7e64f0ba9ba2b61031a571ac961a6f6e4']);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
 $apiResult = curl_exec($ch);
 
 
 curl_close($ch);
 
-//var_dump($apiResult);die;
+$apiResultData = json_decode($apiResult, true);
+
+$data = array_slice($apiResultData, 0, 20);
+
+echo json_encode($data, true);
 
 
 
-//$apiResultData = json_encode ($apiResult, true);
 
-$apiResultData = json_decode ($apiResult, true);
-$cutDownArray = array_slice($apiResultData, 0, 10);
 
+//echo '<pre>';
+//print_r($apiResult);die;
+//echo '</pre>';
+//$apiResultData = $apiResult;
+//$apiResultData = json_decode ($apiResult, true);
+
+//$serializedTest = serialize($apiResultData);
+
+//$apiResultData = json_decode($apiResult, true);
+//$cutDownArray = array_slice($apiResultData, 0, 10);
+//$secondCutDown = array_slice($apiResultData, 10, 10);
+
+//print_r($secondCutDown);die;
 
 //$sauce = json_encode($apiResultData);
 
