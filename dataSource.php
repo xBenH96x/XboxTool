add8f8a8f2784db4308962f4e26cf44ece655c55
xBenH96x <benh96x@gmail.com>
2018-05-29 17:12:47 +0100

    Update dataSource.php
    Removed shit

--- dataSource.php
+++ dataSource.php
@@ -17,39 +17,4 @@ $data = array_slice($apiResultData, 0, 20);
 
 echo json_encode($data, true);
 
-
-
-
-
-//echo '<pre>';
-//print_r($apiResult);die;
-//echo '</pre>';
-//$apiResultData = $apiResult;
-//$apiResultData = json_decode ($apiResult, true);
-
-//$serializedTest = serialize($apiResultData);
-
-//$apiResultData = json_decode($apiResult, true);
-//$cutDownArray = array_slice($apiResultData, 0, 10);
-//$secondCutDown = array_slice($apiResultData, 10, 10);
-
-//print_r($secondCutDown);die;
-
-//$sauce = json_encode($apiResultData);
-
-//echo $apiResultData;die;
-
-//var_dump($apiResultData);
-
-//$length = count($apiResultData);
-
-//for ($i=0; $i < $length ; $i++) {
-
-//print_r ($apiResultData[$i]);
-
-
-//}
-
-//($apiResultData);
-
  ?>
